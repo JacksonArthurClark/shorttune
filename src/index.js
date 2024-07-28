8a489fa9efb6ddd9819ed0e6c2c24b85637076db
Jackson Clark <jclark58@illinois.edu>
2024-07-28 17:42:01 -0500

    Updated port again.

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const isValidYouTubeUrl = (url) => {
 };
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 const acr = new acrcloud({
   host: process.env.ACRCLOUD_HOST,
